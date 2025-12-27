9ed5a4271f81556c076fd19118d399ad8cbd82db
Chris <36350158+choarf@users.noreply.github.com>
2025-12-27 15:44:21 -0500

    Update config.js
    cognito 2

--- js/config.js
+++ js/config.js
@@ -1,11 +1,12 @@
 window._config = {
     cognito: {
-        userPoolId: 'us-east-1_y9C9tTGrH', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '742i50m730csncseh7ik33ms6t', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        userPoolId: 'us-east-1_AdEvGUYb5', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '295567asksk0d6svd8fbgt2oto', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: 'us-east-1' // e.g. us-east-2
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
     }
 };
+
 
